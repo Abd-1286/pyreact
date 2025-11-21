aad34c35da0bfcc10feeb092406af06b0dbf23fd
Abd-1286 <abdullahrafiq.1286@gmail.com>
2025-11-21 15:13:46 +0400

    project complete

--- two/src/Components/Navbar.js
+++ two/src/Components/Navbar.js
@@ -1,23 +1,23 @@
 function Navbar(){
     return(
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <a className="navbar-brand" href="/">Navbar</a>
+        <a className="navbar-brand" href="/App.js">Navbar</a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav">
             <li className="nav-item active">
-                <a className="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+                <a className="nav-link" href="/App.js">Home <span class="sr-only">(current)</span></a>
             </li>
             <li className="nav-item">
-                <a className="nav-link" href="/">Features</a>
+                <a className="nav-link" href="/App.js">Features</a>
             </li>
             <li className="nav-item">
-                <a className="nav-link" href="/">Pricing</a>
+                <a className="nav-link" href="/App.js">Pricing</a>
             </li>
             <li className="nav-item">
-                <a className="nav-link disabled" href="/">Disabled</a>
+                <a className="nav-link disabled" href="/App.js">Disabled</a>
             </li>
             </ul>
         </div>
